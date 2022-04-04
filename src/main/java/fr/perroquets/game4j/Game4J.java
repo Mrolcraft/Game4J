dc46a802c00681505c8e5dd37630810eff0da088
Mrolcraft <Mrolcraft@gmail.com>
2022-04-04 17:26:59 +0200

    add carte

--- src/main/java/fr/perroquets/game4j/Game4J.java
+++ src/main/java/fr/perroquets/game4j/Game4J.java
@@ -5,5 +5,4 @@ public class Game4J {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
 }
---
+++ src/main/java/fr/perroquets/game4j/carte/Carte.java
@@ -0,0 +1,43 @@
+package fr.perroquets.game4j.carte;
+
+public class Carte {
+
+    private int dimensions;
+
+    public Carte(int dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public int getDimensions() {
+        return dimensions;
+    }
+
+    /**
+     * TODO:
+     * Generate map
+     */
+    public void generateCarte() {
+
+    }
+
+    public void afficherCarte() {
+
+    }
+
+    public void addCase() {
+
+    }
+
+    public void removeCase() {
+
+    }
+
+    public void generate_matrix_distance() {
+
+    }
+
+    public void generate_matrix_energie() {
+
+    }
+
+}
