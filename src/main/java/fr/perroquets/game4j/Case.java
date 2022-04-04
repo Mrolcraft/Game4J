a85bdeb4d14b44850c3b37f5755a84750e1b9c9c
Renaud <renaudboulay9@gmail.com>
2022-04-04 18:02:12 +0200

    BRAVO MONSIEUE

--- src/main/java/fr/perroquets/game4j/Case.java
+++ src/main/java/fr/perroquets/game4j/Case.java
@@ -20,5 +20,62 @@ public class Case {
 
     }
 
-    
+    public Case getNorth() {
+        return Game4J.getInstance().getCarte().getCases().stream().filter(aCase ->
+                (aCase.getPosition()[0] == this.x) && (aCase.getPosition()[1] == this.y + 1)
+        ).findFirst().orElse(null);
+    }
+
+    public Case getSouth() {
+        return Game4J.getInstance().getCarte().getCases().stream().filter(aCase ->
+            (aCase.getPosition()[0] == this.x) && (aCase.getPosition()[1] == this.y - 1)
+        ).findFirst().orElse(null);
+
+    }
+
+    public Case getWest() {
+        return Game4J.getInstance().getCarte().getCases().stream().filter(aCase ->
+                (aCase.getPosition()[0] == this.x -1) && (aCase.getPosition()[1] == this.y)
+        ).findFirst().orElse(null);
+
+    }
+
+    public Case getEast() {
+        return Game4J.getInstance().getCarte().getCases().stream().filter(aCase ->
+                (aCase.getPosition()[0] == this.x + 1) && (aCase.getPosition()[1] == this.y)
+        ).findFirst().orElse(null);
+    }
+    public double getDistance(Case caseo) {
+        return (Math.sqrt(Math.pow(caseo.getPosition()[0]-this.x,2) + Math.pow(caseo.getPosition()[1]-this.y,2)));
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
